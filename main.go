468aebc240820a6d23c98471d728c4e357802536
youwalther65 <jens-uwe.walther@web.de>
2020-09-21 19:07:00 +0200

    Update main.go for workshop
    chnaged hello world message

--- main.go
+++ main.go
@@ -33,7 +33,7 @@ func main() {
 
 		io.WriteString(w, string(out))
 
-		fmt.Println("Hello world - the log message")
+		fmt.Println("Hello world from EKS and CodePipeline - the log message")
 	})
 	http.ListenAndServe(":8080", nil)
 }
